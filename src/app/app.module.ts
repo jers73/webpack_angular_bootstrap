d8f52afe66a9a05cf03e59618f9b054d2ecd2b9e
Jorge Reyes <jers73@users.noreply.github.com>
2017-02-15 22:12:59 -0800

    adding ng-boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [ AppComponent ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        NgbModule.forRoot()
     ],
     providers: [ ]
 })
