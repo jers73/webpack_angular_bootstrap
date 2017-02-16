cba137eeb0448662326a973e5713c2d5d3626c70
Jorge Reyes <jers73@gmail.com>
2017-02-15 22:24:51 -0800

    drag and drop example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DndModule } from 'ng2-dnd';
+
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [ AppComponent ],
     imports: [
         BrowserModule,
-        NgbModule.forRoot()
+        NgbModule.forRoot(),
+        DndModule.forRoot()
     ],
     providers: [ ]
 })
