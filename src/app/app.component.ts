8e7a9e5d610d8e4bcdae3e9bdab9dda0af19b0d1
Jorge Reyes <jers73@gmail.com>
2017-02-27 21:30:21 -0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: '[app]',
     template: `
-<div>
+<div class="container">
     <h1>test</h1>
+    <div>Jorge</div>
 </div>
   `
 })
